f48c25be5f1b918721d27ab6e5ed68ee55d0bedb
sky <zhang.yun@ctgtmo.com>
2021-03-01 18:00:08 +0800

    update

--- pages/userMenu/userOrder/userOrder.js
+++ pages/userMenu/userOrder/userOrder.js
@@ -10,6 +10,8 @@ Component({
     }
   },
   data: {
+    navTab: ['进行中', '已完成'],        
+    currentTab: 0,
     userName: '首页',
     leftIndex: 0,
     leftTab: [{title: '摩托车'},{title: '汽车'}],
@@ -24,11 +26,30 @@ Component({
       })
       console.log(e);
     },
+    // 取消订单
+    cancelOrder(){
+      console.log('订单取消');
+    },
     // 进入详情
     toDetail(){
       wx.navigateTo({
         url: '/pages/detail/detail?id='
       })
+    },
+    currentTab: function (e) {
+      if (this.data.currentTab == e.currentTarget.dataset.idx){
+        return;
+      }
+      this.setData({
+        currentTab: e.currentTarget.dataset.idx
+      })
+      this.select={
+        page: 1,
+        size: 6,
+        isEnd: false
+      }
+      this.data.sendList=[];
+      this.getData()
     },
     bindRegionChange: function (e) {
       console.log('picker发送选择改变，携带值为', e.detail.value)
