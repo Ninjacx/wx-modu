e4507adf039389cc2df8a70ca5ec27ba6a6d6a2c
[sky] <[zhang.yun@ctgtmo.com]>
2021-04-09 17:39:41 +0800

    update

--- pages/userMenu/historyOrder/historyOrder.js
+++ pages/userMenu/historyOrder/historyOrder.js
@@ -1,6 +1,9 @@
+import API from '../../../api/index'
+import {beaseUrl} from '../../../request/config'
 Component({
   pageLifetimes: {
     show() {
+      this.init(1)
       if (typeof this.getTabBar === 'function' &&
         this.getTabBar()) {
         this.getTabBar().setData({
@@ -10,25 +13,50 @@ Component({
     }
   },
   data: {
-    userName: '首页',
-    leftIndex: 0,
-    leftTab: [{title: '摩托车'},{title: '汽车'}],
+    beaseUrl: beaseUrl,
+    pageData: [],
+    navTab: [{statusName: '进行中',status: 1},{statusName:'已完成',status: 2}],        
+    currentTab: 1,
+    // userName: '首页',
+    // leftIndex: 0,
+    // leftTab: [{title: '摩托车'},{title: '汽车'}],
     region: ['上海市', '上海市', '浦东新区'],
   }, // 私有数据，可用于模板渲染
  
   methods: {
     // 点击左边菜单
-    leftMenu(e){
-      this.setData({
-        leftIndex: e.currentTarget.dataset.index
+    // leftMenu(e){
+    //   this.setData({
+    //     leftIndex: e.currentTarget.dataset.index
+    //   })
+    //   console.log(e);
+    // },
+    init(status){
+      API.getUserOrderList({status}).then(res => {
+        this.setData({
+          pageData: res.data
+        })
+        // console.log('pageData',this.data.pageData[2]['Publish.volume']);
       })
-      console.log(e);
+    },
+    // 取消订单
+    cancelOrder(){
+      console.log('订单取消');
     },
     // 进入详情
     toDetail(){
       wx.navigateTo({
         url: '/pages/detail/detail?id='
       })
+    },
+    currentTab: function (e) {
+      if (this.data.currentTab == e.currentTarget.dataset.idx){
+        return false;
+      }
+      this.setData({
+        currentTab: e.currentTarget.dataset.idx
+      })
+      this.init(this.data.currentTab)
     },
     bindRegionChange: function (e) {
       console.log('picker发送选择改变，携带值为', e.detail.value)
--- pages/userMenu/userOrder/userOrder.js
+++ pages/userMenu/userOrder/userOrder.js
@@ -1,15 +1,9 @@
 import API from '../../../api/index'
+import {beaseUrl} from '../../../request/config'
 Component({
   pageLifetimes: {
     show() {
-
-      API.getUserOrderList({}).then(res => {
-        this.setData({
-          pageData: res.data
-        })
-        console.log('pageData',this.data.pageData);
-      })
-
+      this.init(1)
       if (typeof this.getTabBar === 'function' &&
         this.getTabBar()) {
         this.getTabBar().setData({
@@ -19,9 +13,10 @@ Component({
     }
   },
   data: {
+    beaseUrl: beaseUrl,
     pageData: [],
-    navTab: ['进行中', '已完成'],        
-    // currentTab: 0,
+    navTab: [{statusName: '进行中',status: 1},{statusName:'已完成',status: 2}],        
+    currentTab: 1,
     // userName: '首页',
     // leftIndex: 0,
     // leftTab: [{title: '摩托车'},{title: '汽车'}],
@@ -30,11 +25,19 @@ Component({
  
   methods: {
     // 点击左边菜单
-    leftMenu(e){
-      this.setData({
-        leftIndex: e.currentTarget.dataset.index
+    // leftMenu(e){
+    //   this.setData({
+    //     leftIndex: e.currentTarget.dataset.index
+    //   })
+    //   console.log(e);
+    // },
+    init(status){
+      API.getUserOrderList({status}).then(res => {
+        this.setData({
+          pageData: res.data
+        })
+        // console.log('pageData',this.data.pageData[2]['Publish.volume']);
       })
-      console.log(e);
     },
     // 取消订单
     cancelOrder(){
@@ -48,26 +51,12 @@ Component({
     },
     currentTab: function (e) {
       if (this.data.currentTab == e.currentTarget.dataset.idx){
-        return;
-      }
-      // console.log('e.currentTarget.dataset.idx',e.currentTarget.dataset.idx);
-      if (e.currentTarget.dataset.idx === 0) {
-        // 请求进行中接口
-        console.log(0);
-      }else if (e.currentTarget.dataset.idx === 1) {
-        // 请求已完成订单
-        console.log(1)
+        return false;
       }
       this.setData({
         currentTab: e.currentTarget.dataset.idx
       })
-      this.select={
-        page: 1,
-        size: 6,
-        isEnd: false
-      }
-      this.data.sendList=[];
-      // this.getData()
+      this.init(this.data.currentTab)
     },
     bindRegionChange: function (e) {
       console.log('picker发送选择改变，携带值为', e.detail.value)
