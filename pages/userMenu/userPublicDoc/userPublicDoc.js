3d98cff7cb003b28f4dcdb0a34cce04aca87e7f2
[sky] <[zhang.yun@ctgtmo.com]>
2021-04-28 18:16:26 +0800

    update

--- pages/userMenu/userPublicDoc/userPublicDoc.js
+++ pages/userMenu/userPublicDoc/userPublicDoc.js
@@ -1,24 +1,36 @@
 import API from '../../../api/index'
+import {setResultList} from '../../../utils/common'
 Page({
   onLoad: function () {
     API.getFindOneUser().then(res=>{
-      this.setState({
-        pageData: res.data
+      // console.log('onload', res.data);
+      var result = setResultList(res.data, ['lease_user_type', 'lease_contact', 'lease_contact_phone', 'lease_emergency_contact', 'lease_emergency_phone', 'lease_addr', 'lease_cardA', 'lease_cardB', 'lease_addr_photo'])
+      // this.data.initPageData = JSON.parse(JSON.stringify(result))
+      this.setData({
+        pageData: result
       })
+      console.log('pageData',this.data.pageData);
     })
   },
   data: {
 
     multiArray: [{userTypeName: '个人'},{userTypeName: '商户'}],
     pageData:{
-      lease_user_type: 0, // 默认个人
+      lease_user_type: '',
+      lease_contact: '',
+      lease_contact_phone: '',
+      lease_emergency_contact: '',
+      lease_emergency_phone: '',
+      lease_addr: '',
+      lease_addr_photo: '', // 店面照片
+      lease_cardA: '', // 身份证照片 正面
+      lease_cardB: '', // 身份证照片 反面
     },
     sex: '1', // 0 女 1 男
     userName: '首页',
     leftIndex: 0,
     leftTab: [{title: '摩托车'},{title: '汽车'}],
     region: ['上海市', '上海市', '浦东新区'],
-
     frontDriveCard: '/image/driveCardA.png',// 驾驶证正面照片
     contraryDriveCard: '/image/driveCardB.png',// 驾驶证反面照片
   }, // 私有数据，可用于模板渲染
